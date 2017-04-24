ff1ba5005d2039c66f4720fa1ee8eb1419143ba9
Erich Gamma <egamma@microsoft.com>
2017-04-24 15:21:04 +0200

    make the sample self contained

--- hello.ts
+++ hello.ts
@@ -1,12 +1,3 @@
-
-abstract class Shape {
-    abstract getArea(): number;
-};
-
-class Circle extends Shape {
-}
-                
-
-[1, 2].map(num => console.log(num));
+// let a = 3;
 
 console.log('hello');
